babdc1125ba5f64f21503814498815bb21410260
Jason Resch <jasonresch+github@gmail.com>
2022-09-08 11:25:22 -0500

    Updated directory paths for client certificate generation

--- bin/issue-client-certificates.sh
+++ bin/issue-client-certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export CA_DIR="config/ca/"
-export KEY_DIR="config/server/keys/"
-export CERT_DIR="config/server/certs/"
+export KEY_DIR="config/client/keys/"
+export CERT_DIR="config/client/certs/"
 java -classpath ../pross-server/target/pross-server-1.0-SNAPSHOT.jar com.ibm.pross.server.app.CertificateAuthorityCli $CA_DIR $KEY_DIR $CERT_DIR false
